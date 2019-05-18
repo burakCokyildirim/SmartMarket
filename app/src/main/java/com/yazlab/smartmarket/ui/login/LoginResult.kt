8fc80f8d75be85d123dda0bee4242084decc287e
Burak COKYILDIRIM <burak.cokyildirim@ctech.com.tr>
2019-05-19 01:23:41 +0300

    First commit

---
+++ app/src/main/java/com/yazlab/smartmarket/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.yazlab.smartmarket.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
